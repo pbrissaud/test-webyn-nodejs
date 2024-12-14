ccb3b488f14090200ff51f3700a6aa3e7e3091f0
Paul Brissaud <pbrissaud.pro@gmail.com>
2024-12-14 15:10:47 +0100

    fix: add google-beta provider
    Signed-off-by: Paul Brissaud <pbrissaud.pro@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "6.13.0"
     }
+    google-beta = {
+      source = "hashicorp/google-beta"
+      version = "6.13.0"
+    }
     github = {
       source = "integrations/github"
       version = "6.4.0"
@@ -12,6 +16,12 @@ terraform {
 }
 
 provider "google" {
+  project     = var.project_id
+  region      = var.region
+  credentials = var.google_credentials
+}
+
+provider "google-beta" {
   project     = var.project_id
   region      = var.region
   credentials = var.google_credentials
