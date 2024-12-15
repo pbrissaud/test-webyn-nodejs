fe664b8dc496e612d5b005d5c4d01fac5e5cab6b
Paul Brissaud <pbrissaud.pro@gmail.com>
2024-12-15 23:18:35 +0100

    feat(CD): Automatic blue/green deployment
    Signed-off-by: Paul Brissaud <pbrissaud.pro@gmail.com>

---
+++ .github/workflows/k6s.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+// Configuration du test
+export let options = {
+  stages: [
+    { duration: '1m', target: 10 },
+    { duration: '2m', target: 20 },
+    { duration: '1m', target: 0 },
+  ],
+};
+
+export default function () {
+  const url = `${__ENV.URL}`;
+  const res = http.get(url);
+
+  // Vérifiez que le service répond correctement
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'response time < 200ms': (r) => r.timings.duration < 200,
+  });
+
+  sleep(1); // Pause entre les requêtes
+}
