c012a6fe6ecc5ad7fc46125d959fe52e9397bd7e
Paul Brissaud <pbrissaud.pro@gmail.com>
2024-12-16 01:04:08 +0100

    fix(k6s): update load test configuration
    Signed-off-by: Paul Brissaud <pbrissaud.pro@gmail.com>

--- k6s.js
+++ k6s.js
@@ -1,24 +1,21 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-// Configuration du test
 export let options = {
-  stages: [
-    { duration: '1m', target: 10 },
-    { duration: '2m', target: 20 },
-    { duration: '1m', target: 0 },
-  ],
+  vus: 50,
+  duration: '30s',
+  thresholds: {
+    checks: ['rate>0.95'],
+  },
 };
 
 export default function () {
   const url = `${__ENV.URL}`;
   const res = http.get(`${url}/hello`);
 
-  // Vérifiez que le service répond correctement
   check(res, {
     'status is 200': (r) => r.status === 200,
-    'response time < 200ms': (r) => r.timings.duration < 200,
   });
 
-  sleep(1); // Pause entre les requêtes
+  sleep(1);
 }
