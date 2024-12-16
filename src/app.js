5e8ed7cf4c6f9d206fe3cd31991e8effeda4dbc0
Paul Brissaud <pbrissaud.pro@gmail.com>
2024-12-16 01:32:55 +0100

    feat(app): simulate internal server error in /hello endpoint
    Signed-off-by: Paul Brissaud <pbrissaud.pro@gmail.com>

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const packageInfo = require("../package.json");
 
 function createApp() {
   app.get("/hello", (_, res) => {
+    if (Math.random() < 0.05) {
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
     res.json({ message: "Hello World!", version: packageInfo.version });
   });
 
